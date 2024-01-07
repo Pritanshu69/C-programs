0cfd005eab900ba46dec0fbcb0e08dc1517cb18b
Pritanshu69 <samantaprit1924@gmail.com>
2024-01-07 12:27:41 +0530

    change

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,15 +1,45 @@
-#include <bits/stdc++.h> 
-void bubbleSort(vector<int>& arr, int n)
-{   
-    for(int i=1;i<n;i++){
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n) {
+    for (int i = 1; i < n; i++) {
         bool swapped = false;
-        for(int j=0;j<n-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j] , arr[j+1]);
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        if (swapped == false) 
+        if (!swapped)
             break;
     }
 }
+
+int main() {
+    int n;
+
+    // User input section
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Sorting the array using bubble sort
+    bubbleSort(arr, n);
+
+    // Printing the sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
